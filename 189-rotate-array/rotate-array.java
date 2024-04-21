110c07d3b087c4dff20d5d175f74034a3f0ee874
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-04-21 13:27:50 +0530

    Time: 1 ms (49.15%) | Memory: 57.7 MB (10.77%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,6 +1,83 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        /*
+        
+        
+
+          //  Brute Force Solution : LEFT ROTATE 
+          /*
+            int n = nums.length;
+            k = k%n;
+
+            int temp[] = new int[k];
+
+            for(int i =0;i<k;i++)
+            {
+                temp[i]=nums[i];
+            }
+          for(int i = k;i<n;i++)
+            {
+                    nums[i-k]=nums[i];
+            }
+
+            for(int i = n-k;i<n;i++)
+            {
+                nums[i]=temp[i-(n-k)];
+            }
+
+
+*/
+
+
+// Brute Force : RIGHT ROTATE
+
+        int n = nums.length;
+        k = k%n;
+        int i;
+       // int s = k+1;
+        int s = n-k;
+        int temp[]=new int[n-k];
+
+        for(i=0;i<s;i++)
+        {
+            temp[i]=nums[i];
+        }
+
+        for(i=n-k;i<n;i++)
+        {
+            nums[i-(n-k)]=nums[i];
+        }
+        int j =0;
+        for(i=k;i<n;i++)
+        {
+            nums[i]=temp[j];
+            j++;
+        }
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /* My Solution : 
             int n = nums.length-1;
             int arr[]=new int[nums.length];
 
@@ -28,18 +105,21 @@ class Solution {
         
 */
        // nums=arr;
+
+       // Optimal Solution : 
+       /*
 int i;
         int arr[]=new int[nums.length];
 
         for(i =0;i<nums.length;i++)
         {
-            arr[(i+k)%nums.length]=nums[i];
+            arr[(i+k)%nums.length]=nums[i];  // 7%7==0
         }
  for(i=0;i<nums.length;i++)
         {
             nums[i]=arr[i];
         }
 
-
+*/
     }
 }
