e71d25f0b28da102d904603dd0485b1414b2b4e9
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-04-22 16:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (92.76%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,7 +1,37 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        
 
+// Optimal Solution : TC : O(N)
+
+
+int n = nums.length;
+
+int sum = (n * (n+1))/2;
+
+
+int i;
+int s=0;
+
+for(i=0;i<nums.length;i++)
+{
+        s=s+nums[i];
+}
+
+int sub = (sum-s);
+return sub;
+
+
+    }
+}
+
+
+
+
+
+
+
+// Brute Force : TC : O(N), SC : (N)
+/*
         int n =nums.length;
 
         int hash[]=new int[n+1];
@@ -19,3 +49,5 @@ class Solution {
         return -1;
     }
 }
+
+*/
