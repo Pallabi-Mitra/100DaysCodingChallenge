3956c8f336873f8882d829720d12e9152edc7ba8
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-04-25 16:37:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (64.54%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,6 +1,9 @@
 class Solution {
     public void sortColors(int[] arr) {
 
+
+// Deutch Flag Algorithm //
+/*
         int n = arr.length;
         int low = 0, mid = 0, high = n - 1; // 3 pointers
 
@@ -26,6 +29,44 @@ class Solution {
                 high--;
             }
         }
+
+*/
+    
+                int n = arr.length;
+                
+               int low = 0;
+               int mid=0;
+               int high=n-1;
+
+
+               while(mid<=high)
+               {
+                    if(arr[mid]==0)
+                    {
+                           int temp=arr[mid];
+                            arr[mid]=arr[low];
+                            arr[low]=temp;
+                            low++;
+                            mid++;
+
+                    }
+                    else if(arr[mid]==1)
+                    {
+                        mid++;
+                    }
+
+                   else if(arr[mid]==2)
+                    {
+                       int temp=arr[mid];
+                        arr[mid]= arr[high];
+                        arr[high]=temp;
+                       high--;
+                    }
+               }
+        // Better Approach 
+
+
+
     }
         
     }
