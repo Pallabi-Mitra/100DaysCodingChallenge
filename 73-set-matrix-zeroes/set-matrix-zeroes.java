085fc31995fd438ba4510c14115271e8fd1ad991
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-04-28 15:01:01 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (56.55%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,7 +2,87 @@ class Solution {
     
     public void setZeroes(int[][] matrix) {
 
-        // Bruteforce :
+
+// Better Solution : 
+
+    
+    int i,j;
+   
+    int n = matrix.length;
+    int m  = matrix[0].length;
+
+    int row[]= new int[n];
+    int col[]=new int[m];
+
+
+
+
+ for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                    if(matrix[i][j]==0)
+                    {
+                       
+                       row[i]=1;
+                       col[j]=1;
+
+                    }
+            }
+        }
+
+
+
+         for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+/*
+        for(i=0;i<n;i++)
+        {
+            j=0;
+            if(row[i]==1)
+            {
+                while(j<m)
+                {
+                matrix[i][j]=0;
+                j++;
+                }
+                
+            }
+        }
+
+
+     for(j=0;j<n;j++)
+        {
+            i=0;
+            if(col[j]==1)
+            {
+                while(i<n)
+                {
+                matrix[i][j]=0;
+                i++;
+                }
+                
+            }
+        }
+
+*/
+
+
+
+
+
+    }
+
+
+
+/*
+        // Bruteforce : O(n*m)*O(n+m)+O(n*m) ~~ n^3
 
         int i,j;
        int row=0,col=0;
@@ -69,7 +149,7 @@ class Solution {
         }
 
 
-
+*/
        
         
     
