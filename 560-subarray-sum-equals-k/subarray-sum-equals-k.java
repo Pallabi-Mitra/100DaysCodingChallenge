2f819e2dfb64044e289c53827125da2cd7c1ee50
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-04-28 22:42:48 +0530

    Time: 25 ms (52.64%) | Memory: 46.5 MB (44.14%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,8 +1,66 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+         int sum = 0;
+        int ans = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int j=0;j<nums.length;j++){
+            sum += nums[j];
+            if(map.containsKey(sum -k)){
+                ans += map.get(sum-k);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return ans;
+    }
+}
+/*
+        int n = arr.length; // size of the given array.
+        Map mpp = new HashMap();
+        int preSum = 0, cnt = 0;
 
+        mpp.put(0, 1); // Setting 0 in the map.
+        for (int i = 0; i < n; i++) {
+            // add current element to prefix Sum:
+            preSum += arr[i];
 
-//bruteforce :
+            // Calculate x-k:
+            int remove = preSum - k;
+
+            // Add the number of subarrays to be removed:
+            cnt += mpp.getOrDefault(remove, 0);
+
+            // Update the count of prefix sum
+            // in the map.
+            mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
+        }
+        return cnt;
+        */
+//Optimal Solution : Prefix sum : O(N)
+
+/*
+int n = nums.length;
+int c=0;
+int sum=0;
+
+HashMap<Integer,Integer> mpp = new HashMap<>();
+mpp.put(0,1);
+for(int i = 0;i<n;i++)
+{
+
+    sum+=nums[i];
+
+    int rem = sum-k;
+
+    c += mpp.getOrDefault(rem, 0);
+    mpp.put(sum,c);
+
+}
+
+    return c;
+
+//bruteforce : O(N2)
+/*
         int n = nums.length;
 
         int c=0;
@@ -25,6 +83,5 @@ class Solution {
         }
 
         return c;
-        
-    }
-}
+       */ 
+ 
