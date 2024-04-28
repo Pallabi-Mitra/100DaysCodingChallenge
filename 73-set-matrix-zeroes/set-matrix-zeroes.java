345cbfdb49c6a1e93af2c74ba44f197dd9b1f689
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-04-28 18:52:37 +0530

    Time: 1 ms (72.36%) | Memory: 45.4 MB (56.55%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -3,8 +3,73 @@ class Solution {
     public void setZeroes(int[][] matrix) {
 
 
-// Better Solution : 
+//Optimal Solution : optimize the space. take the first row, first col.
 
+//int col[m] = {0}; -> matrix[0][..] // first row taken
+//int row[n]={0}; -> matrix[..][0] // first col taken
+int col0=1;
+ int n = matrix.length;
+    int m  = matrix[0].length;
+
+for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                    if(matrix[i][j]==0)
+                    {
+                        // mark the i-th row : 
+                        matrix[i][0]=0;
+
+                        //mark the jth col
+                        if(j!=0)
+                        matrix[0][j]=0;
+                        else
+                        col0=0;
+                      
+
+                    }
+            }    
+        }
+
+
+        for(int i =1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(matrix[i][j]!=0)
+                {
+                    //check for col & row:
+                    if(matrix[0][j]==0 || matrix[i][0]==0)
+                    {
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+
+
+    if(matrix[0][0]==0)
+    {
+        for(int j=0;j<m;j++)
+        {
+            matrix[0][j]=0;
+        }
+
+    }
+    if(col0==0)
+    {
+        for(int i=0;i<n;i++)
+        {
+            matrix[i][0]=0;
+        }
+    }
+
+
+
+
+// Better Solution : TC :O(2*n*m), SC: O(n) + O(m)
+
+/*
     
     int i,j;
    
@@ -41,7 +106,7 @@ class Solution {
             }
         }
 
-/*
+/* not needed :
         for(i=0;i<n;i++)
         {
             j=0;
