89e69e6f17237914c992d59c4588310930777260
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-04-29 20:21:32 +0530

    Time: 3 ms (74.92%) | Memory: 47.9 MB (44.33%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,106 @@
+class Solution {
+    public List<Integer> majorityElement(int[] v) {
+
+
+//Bruteforce :
+/*
+  ArrayList<Integer> al = new ArrayList<>();
+  int n = nums.length;
+  int count=0;
+
+
+  for(int i=0;i<n;i++)
+  {
+        if()
+  }
+
+*/
+int n = v.length; //size of the array
+        List<Integer> ls = new ArrayList<>(); // list of answers
+
+        for (int i = 0; i < n; i++) {
+            //selected element is v[i]:
+            // Checking if v[i] is not already
+            // a part of the answer:
+            if (ls.size() == 0 || ls.get(0) != v[i]) {
+                int cnt = 0;
+                for (int j = 0; j < n; j++) {
+                    // counting the frequency of v[i]
+                    if (v[j] == v[i]) {
+                        cnt++;
+                    }
+                }
+
+                // check if frquency is greater than n/3:
+                if (cnt > (n / 3))
+                    ls.add(v[i]);
+            }
+
+            if (ls.size() == 2) break;
+        }
+
+        return ls;
+    
+
+
+/* my solution : memory limit exceeded : 
+
+        ArrayList<Integer> al = new ArrayList<>();
+        int n = nums.length;
+        int need= n/3;
+        int k=0;
+        if(n==0)
+        {
+            return al;
+        }
+        else if(n==1)
+        {
+            al.add(nums[0]);
+        }
+        else if(n==2)
+        {
+            
+          //  while(nums[k]!=nums[k+1] && k<n)
+            //{
+              //  al.add(nums[k]);
+                // k++;
+            //}
+            
+            while(nums[k]!=nums[k+1])
+            {
+            al.add(nums[0]);
+            al.add(nums[1]);
+            }
+        }
+        else
+        {
+
+                for(int i=0;i<n;i++)
+                {
+                    int count=1;
+                    if(!al.contains(i))
+                    {
+                             for(int j=i+1;j<n;j++)
+                    {
+                        if(nums[i]==nums[j])
+                        {
+                            count++;
+                        }
+                        if(count>need)
+                        {
+                                al.add(nums[i]);
+                                break;
+                        }
+                        
+                    }
+                    }
+                   
+                }
+
+                
+        }
+
+        return al;
+        */
+    }
+}
