18d5621d632e7fe621bb7c59b96830370bcf9ee5
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-04-30 21:24:05 +0530

    Time: 28 ms (87.00%) | Memory: 51.1 MB (84.09%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -3,8 +3,47 @@ class Solution {
 
 //2-pointer approach :
 
-
+/*
     List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+
+        for(int i=0;i<n;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1])
+            continue;
+
+            int j= i+1;
+            int k=n-1;
+
+            while(j<k)
+            {
+                if(nums[i]+nums[j]+nums[k]<0)
+                {
+                    j++;
+                }
+                else if(nums[i]+nums[j]+nums[k]>0)
+                {
+                    k--;
+                }
+                else
+                {
+                    List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k-1]) k--;
+                }
+            }
+        }
+
+
+        return ans;
+
+*/
+List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(arr);
         int n = arr.length;
 
@@ -34,8 +73,6 @@ class Solution {
         }
 
         return ans;
-
-
 
 
 
