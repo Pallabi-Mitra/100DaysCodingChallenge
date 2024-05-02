261a87280ce2afa1b615579fc1c1362e0def9f8f
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-02 18:17:55 +0530

    Time: 17 ms (67.18%) | Memory: 43.6 MB (88.54%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,6 +1,112 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+
+//Optimal Solution : O()
 /*
+int n = nums.length;
+List<List<Integer>> ans = new ArrayList<>();
+Arrays.sort(nums);
+
+for(int i=0;i<n;i++)
+{
+    if(i!=0 && nums[i]==nums[i-1])continue; // skip duplicates
+
+    for(int j=i+1;j<n;j++)
+    {
+      if(j!=i+1 && nums[j]==nums[j-1])continue; // skip duplicates
+
+     int k = j+1;
+        int m = n-1;
+
+        while(k<m)
+        {
+                
+                long sum= nums[i];
+                sum+=nums[j];
+                sum+=nums[k];
+                sum+=nums[m];
+
+                if(sum==target)
+                {
+                    List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]+nums[m]);
+                    ans.add(temp);
+                    k++;
+                    m--;
+                    while(k<m && nums[k]==nums[k-1])k++;
+                    while(k<m && nums[m]==nums[m+1])m--;
+                }
+                else if(sum<target)
+                {
+                    k++;
+                }
+                else
+                {
+                    m--;
+                }
+
+        }
+
+
+
+
+    }
+}
+
+    return ans;
+
+
+    }
+}
+*/
+int n = nums.length; // size of the array
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // sort the given array:
+        Arrays.sort(nums);
+
+        // calculating the quadruplets:
+        for (int i = 0; i < n; i++) {
+            // avoid the duplicates while moving i:
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                // avoid the duplicates while moving j:
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                // 2 pointers:
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+
+                        // skip the duplicates:
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+                    } else if (sum < target) k++;
+                    else l--;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+
+}
+
+
+/*//Better Solution : O(n^3)
 int n = nums.length; // size of the array
         Set<List<Integer>> st = new HashSet<>();
 
@@ -35,7 +141,7 @@ int n = nums.length; // size of the array
 */
 //Better Solution : O(n^3)
 
-
+/*
 int n = nums.length;
 
 Set<List<Integer>> st = new HashSet<>();
@@ -70,7 +176,7 @@ for(int i = 0;i<n;i++)
     }
 }
 
-
+*/
 
 
         //Brute force : T.C :O(n^4), S.C : 0(1)
