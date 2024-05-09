1a7a8a8626541818abbfe1e08aba46025e9e4a8f
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-10 02:20:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (79.07%) - LeetSync

--- 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -1,11 +1,20 @@
 class Solution {
     public int findMin(int[] nums) {
-        
+
+
+// Optimized Solution : 
+
+
+
+
+
+      // Bruteforce  :  
         int m = 1000000;
         for (int i : nums) {
             m = Math.min(m, i);
         }
         return m;
+        
         
     }
 }
