178f7401bc9012fdaaf9276c23e2776df6a5a1f5
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-11 00:57:07 +0530

    Time: 12 ms (34.10%) | Memory: 46.1 MB (99.74%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,76 @@
+class Solution {
+
+    public int calculateWeight(int a[], int mid)
+    {
+        int total=0;
+        int count=1;
+        /*
+        for(int i=0;i<a.length;i++)
+        {
+           
+            if(total<mid)
+            {
+                 total+=a[i];
+            }
+            else
+            {
+                count++;
+                total=0;
+            }
+            return count;
+        }
+*/
+int i =0;
+int m = a.length;
+        while(total<=mid && i<m)
+        {
+             total+=a[i];
+             
+                    if(total>mid)
+                {
+                    count++;
+                    total=0;
+                    i--;
+                }
+                i++;
+        }
+        return count;
+        
+    }
+    public int shipWithinDays(int[] weights, int days) {
+
+
+        int n = weights.length;
+        int maxi = Integer.MIN_VALUE;
+        int sum=0;
+
+        for(int i = 0;i<n;i++)
+        {
+            maxi = Math.max(maxi,weights[i]);
+            sum+=weights[i];
+        }
+
+        int low = maxi;
+        int high = sum;
+        int ans =-1;
+
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            int res = calculateWeight(weights, mid);
+
+            if(res<=days)
+            {
+                ans=mid;
+               high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        
+        return ans;
+        
+    }
+}
