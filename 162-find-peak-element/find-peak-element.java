0a8f637101dde26db5d98d044cf0a33b29925400
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-10 15:09:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (64.25%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,6 +1,70 @@
 class Solution {
     public int findPeakElement(int[] nums) {
 
+// Optimized : Binary Search : O(log n):
+
+int n = nums.length;
+int low = 1;
+int high = n-2;
+
+if(n==1) return 0;
+
+for(int i =0;i<n;i++) // first and last element
+{
+
+    if((i==0 || nums[i]>nums[i-1] ) && (i==n-1 || nums[i]>nums[i+1]))
+    {
+        return i;
+    }
+    /*
+    if(i==0)
+    {
+        if(nums[i+1]<nums[i])
+        {
+            return i;
+        }
+    }
+    else if(i==n-1)
+    {
+        if(nums[i-1]<nums[i])
+        {
+            return i;
+        }
+    }
+    */
+}
+// binary search
+          while(low<=high)
+            {
+                int mid = (low+high)/2;
+
+                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+                {
+                    return mid;
+                }
+                // increasing curve : 
+                else if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1])
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
+            }
+    
+
+return -1;
+    }
+}
+
+
+
+
+
+
+// Bruteforce : O(n):
+/*
         int n = nums.length;
 
         for(int i = 0;i<n;i++)
@@ -15,3 +79,4 @@ class Solution {
         
     }
 }
+*/
