68e6199a98e3e2d8e6dbd0b0b731ea6dc5e3c60f
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-10 11:30:19 +0530

    Time: 0 ms (100.00%) | Memory: 49.8 MB (91.27%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,47 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
 
-        int n= nums.length;
-        int index=0;
+        
        
+// Optimized : Binary Search : O(log n):
+int n= nums.length;
+
+if(n==1) return nums[0];
+if(nums[0]!=nums[1]) return nums[0];
+if(nums[n-1]!=nums[n-2]) return nums[n-1];
+
+int low = 1;
+int high = n-2;
+
+    while(low<=high)
+    {
+        int mid =(low+high)/2;
+
+        if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) // if mid is not same with
+         //either left or right then it is the single element
+        {
+            return nums[mid];
+        }
+
+        // elimination part : 
+
+        if((mid%2==1 && nums[mid-1] == nums[mid]) || (mid%2==0 && nums[mid]==nums[mid+1]))
+        {
+            low=mid+1; // eliminate left half
+        }
+        else
+        {
+            high = mid-1; // eliminate right half;
+        }
+
+    }
+    return -1;
+    }
+}
+
+
+// Bruteforce : O(n):
+/*
 
         if(n==1)
         {
@@ -23,7 +61,8 @@ class Solution {
                 }
                 else
                 {
-                    if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1])
+                    if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]) // either left or right 
+                    //should not have the element
                     {
                         return nums[i];
                     }
@@ -34,3 +73,4 @@ class Solution {
         
     }
 }
+*/
