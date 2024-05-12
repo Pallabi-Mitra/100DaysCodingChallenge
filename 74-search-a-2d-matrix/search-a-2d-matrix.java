5df360e6be64e96a2063b09fb37207f0767f1f9a
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-12 13:49:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (22.66%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,8 +1,48 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
+// Optimized : Binary Search :
+
     int n = matrix.length;
     int m = matrix[0].length;
+
+    for(int i = 0;i<n;i++)
+    {
+        if(matrix[i][0] <= target && matrix[i][m-1] >= target)
+        {
+            // start binary search :
+
+            int low = 0;
+            int high = m-1;
+
+            while(low<=high)
+            {
+                int mid = (low+high)/2;
+
+                if(matrix[i][mid]==target)
+                {
+                    return true;
+                }
+                else if(matrix[i][mid]<target)
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high = mid-1;
+                }
+            }
+        }
+        
+    }
+    return false;
+
+
+
+
+
+
+/* Bruteforce :
 
     for(int i  =0 ; i<n;i++)
     {
@@ -15,6 +55,6 @@ class Solution {
         }
     }
     return false;
-        
+   */     
     }
 }
