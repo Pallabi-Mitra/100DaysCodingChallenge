a034b6027388f033e34ea15e91e94a74374d1691
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-15 01:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (80.59%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -13,43 +13,11 @@
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
 
-// Bruteforce : 
-
-    ListNode temp= head;
-    int cnt = 0;
-    while(temp!=null)
-    {
-        cnt++;
-        temp=temp.next;
-    }
-
-    if(cnt == n)
-    {
-        ListNode newhead = head.next;
-        head=null;
-        return newhead;
-    }
-    
-    temp=head;
-    int res= cnt-n;
-    while(temp!=null)
-    {
-        res--;
-        if(res==0)
-        {
-            break;
-           
-        }
-        temp=temp.next;
-    }
-     temp.next=temp.next.next;
-
-return head;
-    }
-}
 
 
-     /*   
+    //    Optimized :
+
+
         ListNode fast= head;
         ListNode slow = head;
 
@@ -64,15 +32,51 @@ return head;
             ListNode newHead = head.next;
             return newHead;
         }
-         while(fast.next!=null)
+         while(fast.next!=null) // last node
         {
             slow=slow.next;
             fast=fast.next;
         }
-        ListNode delNode = slow.next;
+       // ListNode delNode = slow.next;
         slow.next=slow.next.next;
         return head;
     }
 }
 
+/*
+// Bruteforce : 
+
+    ListNode temp= head;
+    int cnt = 0;
+    while(temp!=null)
+    {
+        cnt++; // getting the length of the LL
+        temp=temp.next;
+    }
+
+    if(cnt == n) // if head is asked to delete
+    {
+        ListNode newhead = head.next;
+        head=null;
+        return newhead;
+    }
+    
+    temp=head;
+    int res= cnt-n; // we get the node 1  before we need to delete
+    while(temp!=null)
+    {
+        res--;// we traverse the LL till the node
+        if(res==0)
+        {
+            break;
+           
+        }
+        temp=temp.next;
+    }
+     temp.next=temp.next.next; //  we make the node point to one node ahead, deleting the 
+     // middle skipped node which was asked to be deleted
+
+return head;
+    }
+}
 */
