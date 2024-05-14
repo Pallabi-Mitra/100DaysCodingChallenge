969f703bcd3effe424f607da67dd301397d781e0
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-15 02:54:05 +0530

    Time: 3 ms (99.79%) | Memory: 62.8 MB (89.58%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.java
@@ -11,7 +11,32 @@
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
 
-        if(head==null)return head;
+
+// Optimized :tortoise hare algorithm
+
+if(head==null || head.next==null) return null;
+ListNode slow= head;
+ListNode fast=head;
+
+int cnt = 0;
+fast=fast.next.next;
+    while(fast!=null && fast.next!=null)
+    {
+
+        slow=slow.next;
+        fast=fast.next.next;
+       
+    }
+    slow.next=slow.next.next;
+
+    return head;
+    }
+}
+
+
+//Bruteforce :
+/*
+        if(head==null)return head; // no node
         ListNode temp= head;
         int cnt = 0;
 
@@ -20,7 +45,7 @@ class Solution {
             cnt++;
             temp=temp.next;
         }
-        if(cnt==1)
+        if(cnt==1) // if head
         {
             head=head.next;
             return head;
@@ -33,13 +58,14 @@ class Solution {
         while(temp!=null)
         {
             res--;
-            if(res==0)
+            if(res==0) // to reach to the node before the middle node to be deleted
             break;
 
             temp=temp.next;
         }
 
-        temp.next=temp.next.next;
+        temp.next=temp.next.next; // delete the node, point the prev node to the next 
         return head;
     }
 }
+*/
