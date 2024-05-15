a4df645207bfa96ebda3db6ce5141948887d4468
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-15 15:44:44 +0530

    Time: 1 ms (99.91%) | Memory: 48.1 MB (77.22%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -12,9 +12,63 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         
+// Optimized:
+
+int n1= 0;
+int n2=0;
+
+    ListNode temp = headA;
+
+    while(temp!=null)
+    {
+        n1++;
+        temp=temp.next;
+    }
+    temp=headB;
+
+while(temp!=null)
+    {
+        n2++;
+        temp=temp.next;
+    }
+
+    if(n1<n2)
+        return intersectionPoint(headA,headB,n2-n1); //smaller head,larger head,diff
+    else
+        return intersectionPoint(headB,headA,n1-n2); 
+    }
+
+    public ListNode intersectionPoint(ListNode headA,ListNode headB,int diff)
+    {
+        // second head is larger so move it by diff dist:
+
+        while(diff!=0)
+        {
+            headB= headB.next;
+            diff--;
+        }
+
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+
+        while(tempA!=null && tempB !=null)
+        {
+            if(tempA==tempB)
+                return tempA;
+            tempA=tempA.next;
+            tempB=tempB.next;
+        }
+
+    return null;
+    }
+
+
+
+}
 
 
         // Bruteforce : Hashing :
+        /*
         if(headA==null|| headB==null )return null;
 
         ListNode temp = headA;
@@ -42,3 +96,5 @@ public class Solution {
         return null;
     }
 }
+
+*/
