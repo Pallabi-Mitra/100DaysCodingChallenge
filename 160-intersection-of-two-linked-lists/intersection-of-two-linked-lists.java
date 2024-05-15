5742c47086b9b650b5c280f9db36d97ce7af9040
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-15 16:25:43 +0530

    Time: 1 ms (99.91%) | Memory: 47.6 MB (92.20%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -12,29 +12,63 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         
-// Optimized:
+// Optimized : O(n1 + n2)
 
-int n1= 0;
-int n2=0;
+if(headA == null || headB ==null) return null;
+
+ListNode temp1 = headA;
+ListNode temp2 = headB;
+
+    while(temp1!= temp2)
+    {
+        temp1 = temp1.next;
+        temp2=temp2.next;
+
+        if(temp1 == temp2) return temp1;
+
+        if(temp1 == null) temp1= headB;
+        if(temp2 == null) temp2= headA;
+
+    }
+    return temp1;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+// Better:O(n1 + 2n2)
+/*
+int n1= 0; // counting elements in LL1
+int n2=0; // counting elements in LL2
 
     ListNode temp = headA;
 
-    while(temp!=null)
+    while(temp!=null) // counting the total no of nodes in LL1
     {
         n1++;
         temp=temp.next;
     }
     temp=headB;
 
-while(temp!=null)
+while(temp!=null) // counting the total no of nodes in LL2
     {
         n2++;
         temp=temp.next;
     }
 
-    if(n1<n2)
+    if(n1<n2) // if LL2 is larger 
+// we send the smaller head first
         return intersectionPoint(headA,headB,n2-n1); //smaller head,larger head,diff
-    else
+    else // LL1 is larger
         return intersectionPoint(headB,headA,n1-n2); 
     }
 
@@ -44,13 +78,14 @@ while(temp!=null)
 
         while(diff!=0)
         {
-            headB= headB.next;
-            diff--;
+            headB= headB.next; // we move the LL which is greater by diff so that we can 
+            //start traversing the shorter LL and larger LL from same node
+            diff--; 
         }
 
         ListNode tempA = headA;
         ListNode tempB = headB;
-/*
+//
         while(tempA!=null && tempB !=null)
         {
             if(tempA==tempB)
@@ -58,21 +93,22 @@ while(temp!=null)
             tempA=tempA.next;
             tempB=tempB.next;
         }
-*/
 
-    while(tempA!=tempB)
+//
+// another way of looping : 
+    while(tempA!=tempB) // untill found same node keep traversing
     {
          tempA=tempA.next;
         tempB=tempB.next;
     }
-    return tempA;
+    return tempA;// when found same return
     }
 
 
 
 }
 
-
+*/
         // Bruteforce : Hashing :
         /*
         if(headA==null|| headB==null )return null;
