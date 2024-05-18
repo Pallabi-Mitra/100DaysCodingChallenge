54a8a101fc242ae9bbe1b85fadabb7ed8c3f0835
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-18 23:58:17 +0530

    Time: 3 ms (100.00%) | Memory: 44.6 MB (64.99%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,3 +1,4 @@
+/*
 class MinStack {
 
 Stack<Integer> stack;
@@ -39,7 +40,43 @@ int min;
         
     }
 }
+*/
+class MinStack {
+    class Node {
+        int val;
+        int min;
+        Node next;
 
+        Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    Node head;
+
+    public MinStack() {
+        head = null;
+    }
+
+    public void push(int val) {
+        int min = head == null ? val : Math.min(val, head.min);
+        head = new Node(val, min, head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+}
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
