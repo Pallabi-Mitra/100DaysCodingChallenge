517b92f12c024596e9ff9434b2be33f441ed1abf
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-18 08:43:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (99.71%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -9,7 +9,65 @@
  * }
  */
 class Solution {
+
+    // Iterative :
+     
+ public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+
    
+        if(head1==null)return head2;
+        if(head2==null) return head1;
+
+        ListNode temp1 = head1;
+        ListNode temp2 = head2;
+
+        ListNode dummyNode = new ListNode(-1);
+
+        ListNode res = dummyNode;
+
+        while(temp1!=null && temp2!=null)
+        {
+            if(temp1.val<temp2.val)
+            {
+                res.next=temp1;
+                res=temp1;
+                temp1=temp1.next;
+            }
+            else 
+            {
+                res.next=temp2;
+                res=temp2;
+                temp2=temp2.next;
+            }
+           
+        }
+        while(temp1!=null)
+        {
+            res.next=temp1;
+            res=temp1;
+            temp1=temp1.next;
+        }
+        while(temp2!=null)
+        {
+            res.next=temp2;
+            res=temp2;
+            temp2=temp2.next;
+        }
+        
+        return dummyNode.next;
+  
+}
+}
+
+     
+    
+    
+
+
+    // Recursion :
+
+     // Recursion : 
+     /*
 public ListNode merge(ListNode head1, ListNode head2) {
  
 
@@ -33,61 +91,4 @@ public ListNode merge(ListNode head1, ListNode head2) {
 
    return merge(head1,head2);
  }
-
-}
-
-
-
-// My code :
-
-/*
-        if(head1==null && head2==null) return null;
-
-        if(head1==null && head2.next==null) return head2; 
-
-        if(head2==null && head1.next==null) return head1;
-
-        ListNode temp1 = head1;
-        ListNode temp2 = head2;
-
-        ListNode dummyNode = new ListNode(-1);
-
-        ListNode res = dummyNode;
-
-        while(temp1!=null || temp2!=null)
-        {
-            if(temp1.val<temp2.val)
-            {
-                res.next=temp1;
-                res=temp1;
-                temp1=temp1.next;
-            }
-            else if(temp2.val<temp1.val)
-            {
-                res.next=temp2;
-                res=temp2;
-                temp2=temp2.next;
-            }
-           // else {// if equal
-            
-            //    res.next=temp1;
-            //    res=temp1;
-            //    temp1=temp1.next;
-           // }
-        }
-        while(temp1!=null)
-        {
-            res.next=temp1;
-            res=temp1;
-            temp1=temp1.next;
-        }
-        while(temp2!=null)
-        {
-            res.next=temp2;
-            res=temp2;
-            temp2=temp2.next;
-        }
-        
-        return dummyNode.next;
-    
-    */
+*/
