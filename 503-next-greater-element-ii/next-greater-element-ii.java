f7453ca797fe06aada3a52fd8be4a0a423fe6154
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-19 16:17:14 +0530

    Time: 17 ms (43.23%) | Memory: 45.9 MB (63.83%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
 
@@ -5,21 +6,21 @@ class Solution {
         Stack<Integer> st = new Stack<>();
 
         int result[] = new int[n];
-
-        for (int i = 2*n; i >= 0; i--) {
+// circular so 2*n to copy the array twice beside, modulo can give us the array index again
+        for (int i = 2*n-1; i >= 0; i--) { // traverse from back
             while (!st.isEmpty() && st.peek() <= nums[i % n]) {
-                st.pop();
+                st.pop(); // if the stcak has element less than current element pop it
             }
 
-            if (i < n) {
-                if (st.isEmpty() == false) {
-                    result[i] = st.peek();
+            if (i < n) { 
+                if (st.isEmpty() == false) { // if stack has elements
+                    result[i] = st.peek(); // the current stack element will be the greater 
                 } else {
-                    result[i] = -1;
+                    result[i] = -1; // if stack empty after popping all smaller put -1 bcoz no greater element found
                 }
             }
 
-            st.push(nums[i % n]);
+            st.push(nums[i % n]); // push all the lements in the stack
         }
 
         return result;
