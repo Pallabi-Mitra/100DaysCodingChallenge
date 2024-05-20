16f672c9f390fbc6c1409f5cdc00901b4afd93e6
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-20 18:39:56 +0530

    Time: 11 ms (93.39%) | Memory: 46.5 MB (27.50%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,8 +1,72 @@
 class Solution {
     public int calculate(String s) {
 
+//Brute force :
 
+          List<Integer> numbers = new ArrayList<>();
+        List<Character> operators = new ArrayList<>();
+        
+        int n = s.length();
+        int i = 0;
+        
+        while (i < n) {
+            char ch = s.charAt(i);
+            
+            if (Character.isDigit(ch)) {
+                int num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                numbers.add(num);
+            } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                operators.add(ch);
+                i++;
+            } else {
+                i++; // skip spaces
+            }
+        }
+        
+        // Handle * and / first
+        for (int j = 0; j < operators.size(); j++) {
+            char op = operators.get(j);
+            if (op == '*' || op == '/') {
+                int left = numbers.get(j);
+                int right = numbers.get(j + 1);
+                int result = 0;
+                
+                if (op == '*') {
+                    result = left * right;
+                } else {
+                    result = left / right;
+                }
+                
+                numbers.set(j, result);
+                numbers.remove(j + 1);
+                operators.remove(j);
+                j--; // adjust index after removal
+            }
+        }
+        
+        // Handle + and -
+        int result = numbers.get(0);
+        for (int j = 0; j < operators.size(); j++) {
+            char op = operators.get(j);
+            int right = numbers.get(j + 1);
+            
+            if (op == '+') {
+                result += right;
+            } else {
+                result -= right;
+            }
+        }
+        
+        return result;
+    }
+}
 
+// Better : Using Stacks
+/*
         Stack<Integer> stack = new Stack<>();
         int currentNumber = 0;
         char operation = '+';
@@ -38,13 +102,13 @@ class Solution {
         return result;
     }
 }
+*/
 
 
 
 
 
-
-   // Optimal :
+   // Optimal : Single pass
    /*    
         int result = 0;
         int lastNumber = 0;
