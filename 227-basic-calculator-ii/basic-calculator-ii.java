6b8be63e9a0f39e515f75688957454bf4283cb65
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-20 18:38:49 +0530

    Time: 26 ms (32.23%) | Memory: 46 MB (59.92%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,7 +1,51 @@
 class Solution {
     public int calculate(String s) {
 
-       
+
+
+        Stack<Integer> stack = new Stack<>();
+        int currentNumber = 0;
+        char operation = '+';
+        
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            
+            if (Character.isDigit(currentChar)) {
+                currentNumber = currentNumber * 10 + (currentChar - '0');
+            }
+            
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == s.length() - 1) {
+                if (operation == '+') {
+                    stack.push(currentNumber);
+                } else if (operation == '-') {
+                    stack.push(-currentNumber);
+                } else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                } else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        
+        return result;
+    }
+}
+
+
+
+
+
+
+   // Optimal :
+   /*    
         int result = 0;
         int lastNumber = 0;
         int currentNumber = 0;
@@ -37,4 +81,4 @@ class Solution {
     }
 }
 
-   
+   */
