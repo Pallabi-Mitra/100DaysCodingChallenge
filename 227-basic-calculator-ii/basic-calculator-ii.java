eef22a0e2003d383e56e33acf3b4940761616654
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-20 19:21:50 +0530

    Time: 14 ms (84.51%) | Memory: 44.7 MB (82.50%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.java
+++ 227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,9 +1,92 @@
 class Solution {
     public int calculate(String s) {
 
-//Brute force :
+// Better : Using Stacks
+/*
+        Stack<Integer> stack = new Stack<>();
+        int currentNumber = 0;
+        char operation = '+';
+        
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            
+            if (Character.isDigit(currentChar)) {
+                currentNumber = currentNumber * 10 + (currentChar - '0');
+            }
+            
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == s.length() - 1) {
+                if (operation == '+') {
+                    stack.push(currentNumber);
+                } else if (operation == '-') {
+                    stack.push(-currentNumber);
+                } else if (operation == '*') {
+                    stack.push(stack.pop() * currentNumber);
+                } else if (operation == '/') {
+                    stack.push(stack.pop() / currentNumber);
+                }
+                
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        
+        return result;
+    }
+}
+*/
 
-          List<Integer> numbers = new ArrayList<>();
+
+
+
+
+   // Optimal : Single pass
+      
+        int result = 0;
+        int lastNumber = 0;
+        int currentNumber = 0;
+        char operation = '+';
+        
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            
+            if (Character.isDigit(currentChar)) {
+                currentNumber = currentNumber * 10 + (currentChar - '0');
+            }
+            
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == s.length() - 1) {
+                if (operation == '+') {
+                    result += lastNumber;
+                    lastNumber = currentNumber;
+                } else if (operation == '-') {
+                    result += lastNumber;
+                    lastNumber = -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+                
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        
+        result += lastNumber;
+        return result;
+    }
+}
+
+   
+
+   
+//Brute force : Using 2 ArrayList
+/*
+        List<Integer> numbers = new ArrayList<>();
         List<Character> operators = new ArrayList<>();
         
         int n = s.length();
@@ -64,85 +147,4 @@ class Solution {
         return result;
     }
 }
-
-// Better : Using Stacks
-/*
-        Stack<Integer> stack = new Stack<>();
-        int currentNumber = 0;
-        char operation = '+';
-        
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            
-            if (Character.isDigit(currentChar)) {
-                currentNumber = currentNumber * 10 + (currentChar - '0');
-            }
-            
-            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == s.length() - 1) {
-                if (operation == '+') {
-                    stack.push(currentNumber);
-                } else if (operation == '-') {
-                    stack.push(-currentNumber);
-                } else if (operation == '*') {
-                    stack.push(stack.pop() * currentNumber);
-                } else if (operation == '/') {
-                    stack.push(stack.pop() / currentNumber);
-                }
-                
-                operation = currentChar;
-                currentNumber = 0;
-            }
-        }
-        
-        int result = 0;
-        while (!stack.isEmpty()) {
-            result += stack.pop();
-        }
-        
-        return result;
-    }
-}
 */
-
-
-
-
-
-   // Optimal : Single pass
-   /*    
-        int result = 0;
-        int lastNumber = 0;
-        int currentNumber = 0;
-        char operation = '+';
-        
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            
-            if (Character.isDigit(currentChar)) {
-                currentNumber = currentNumber * 10 + (currentChar - '0');
-            }
-            
-            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == s.length() - 1) {
-                if (operation == '+') {
-                    result += lastNumber;
-                    lastNumber = currentNumber;
-                } else if (operation == '-') {
-                    result += lastNumber;
-                    lastNumber = -currentNumber;
-                } else if (operation == '*') {
-                    lastNumber = lastNumber * currentNumber;
-                } else if (operation == '/') {
-                    lastNumber = lastNumber / currentNumber;
-                }
-                
-                operation = currentChar;
-                currentNumber = 0;
-            }
-        }
-        
-        result += lastNumber;
-        return result;
-    }
-}
-
-   */
