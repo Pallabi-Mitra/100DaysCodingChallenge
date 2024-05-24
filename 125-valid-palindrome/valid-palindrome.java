a45de328ca9a47dd20538f7a10ec670651c94d34
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-24 23:28:07 +0530

    Time: 3 ms (73.73%) | Memory: 43.4 MB (52.01%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,6 +2,24 @@ class Solution {
     public boolean isPalindrome(String s) {
 
 
+
+StringBuilder sb = new StringBuilder();
+
+    for(char ch : s.toCharArray()){
+        if(Character.isLetterOrDigit(ch)){
+            sb.append(Character.toLowerCase(ch));
+        }
+    }
+    if(sb.toString().equals(sb.reverse().toString())){
+    return true;
+    }
+    return false;
+    }
+}
+
+
+// 2-pointer approach :
+/*
     String ans = "";
 
     for(char c : s.toCharArray())
@@ -26,13 +44,8 @@ class Solution {
     }
 
     return true;
-
-
-
-        
-
-
-
-        
+     
     }
 }
+
+*/
