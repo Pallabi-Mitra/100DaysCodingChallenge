6a7a0905a32e82749160a0155dcdfbfc08141cf6
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-25 16:58:57 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (90.80%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,24 +1,56 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
 
+// sorting the string array,lexicographically, then checking with only first and last string.
+// how much they match will be the max available length of prefix common among all.
+// TC: O(n log n + m):
 
+   
 
-    if (strs == null || strs.length == 0) {
+      
+
+    Arrays.sort(strs); // sorts the string array lexicographically
+      if (strs[0].equals("")|| strs.length == 0) {
             return "";
         }
-        
-        Arrays.sort(strs);
-        String first = strs[0];
-        String last = strs[strs.length - 1];
-        
-        int i = 0;
-        while (i < first.length() && i < last.length() && first.charAt(i) == last.charAt(i)) {
-            i++;
+    int n = strs.length;
+    int i;
+
+    String first = strs[0];
+    String last = strs[n-1];
+
+    int f = strs[0].length();
+    int l = last.length();
+
+    int len = f>l ? l : f;
+    if(first.charAt(0)!=last.charAt(0))
+        {
+            return "";
         }
+
+    for(i = 0;i<len;i++)
+    {
         
-        return first.substring(0, i);
+        if(first.charAt(i)==last.charAt(i))
+        {
+           continue;
+        }
+        else
+        {
+            break;
+        }
     }
-}       
+
+    return first.substring(0,i);
+
+    }
+}
+
+
+
+
+
+
 /*
         String res="";
         int j = 0;
