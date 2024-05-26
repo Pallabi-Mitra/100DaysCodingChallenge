78fad0ba9e74eeace8c936de1b121f18917de148
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-26 16:20:09 +0530

    Time: 16 ms (24.38%) | Memory: 42.1 MB (75.39%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -2,6 +2,8 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
 // Bruteforce :
 
+
+/*
 // Arrays to store character mappings from s to t and t to s
     int[] sMap = new int[256]; // sMap stores mappings from s to t
     int[] tMap = new int[256]; // tMap stores mappings from t to s
@@ -13,6 +15,7 @@ class Solution {
 
         // Check if there is a mismatch in the mappings
         // Using sChar and tChar directly as indices in the arrays
+
         if (sMap[sChar] != tMap[tChar]) {
             return false; // Mismatch found, so not isomorphic
         }
@@ -27,27 +30,47 @@ class Solution {
 }
 }
 
-
+*/
 // Using 2 hashmap :
-/*
-        HashMap<Character,Integer> mppS = new HashMap<>();
+
+        HashMap<Character,Character> mppS = new HashMap<>();
         
-        HashMap<Character,Integer> mppT = new HashMap<>();
+        HashMap<Character,Character> mppT = new HashMap<>();
 
 
         for(int i = 0;i<s.length();i++)
         {
-            mppS.put(s.charAt(i),i);
-            mppT.put(t.charAt(i),i);
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            
+
+            if(mppS.containsKey(sChar))
+            {
+                if(mppS.get(sChar)!=tChar)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                mppS.put(sChar,tChar);
+            }
+
+            if(mppT.containsKey(tChar))
+            {
+                if(mppT.get(tChar)!=sChar)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                mppT.put(tChar,sChar);
+            }
         }
 
-        if(mppS.size()==mppT.size())
-        {
-            return true;
-        }
-        return false;
+       return true;
 
         
     }
 }
-*/
