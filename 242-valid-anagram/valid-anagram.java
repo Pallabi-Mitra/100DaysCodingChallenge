052b8543e7e1ee83535dbab546cdf2647c74269f
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-26 19:28:34 +0530

    Time: 15 ms (27.68%) | Memory: 44.8 MB (16.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isAnagram(String s, String t) {
 
 // By sorting 
+/*
 
     char ch1[]= s.toCharArray();
 
@@ -22,10 +23,61 @@ class Solution {
     return true;
     else
     return false;
-
-
-
-
-        
+    //return Arrays.equals(ch1, ch2);
+      
     }
 }
+
+*/
+
+// Using HashMap :
+
+// 
+
+HashMap<Character,Integer> mpp = new HashMap<>();
+
+
+// put the characters in 1st string along with their frequency in hashmap:
+
+for(char c: s.toCharArray())
+{
+    
+    mpp.put(c,mpp.getOrDefault(c,0)+1);
+
+
+}
+
+// iterate through string 2 and check if something is not present in hashmap or else reduce the count and put it
+
+for(char ch : t.toCharArray())
+{
+    if(!mpp.containsKey(ch))
+        return false;
+    
+    mpp.put(ch,mpp.get(ch)-1);
+}
+
+
+// looping through hashmap to check which is having value other than 0. It needs to be 0 to match completely
+
+
+for(int count : mpp.values())
+{
+    if(count!=0)
+    return false;
+    
+}
+
+return true;
+    }
+}
+
+
+
+
+
+
+// Using Hash Table :
+
+
+
