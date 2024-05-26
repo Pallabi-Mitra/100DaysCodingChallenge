16c09e33593c4b8696d8062af28d90d8e9d5a8f4
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-27 01:34:53 +0530

    Time: 6 ms (6.41%) | Memory: 42.6 MB (5.61%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
 
+/*
+//An optimized approach would be to concatenate the original string with itself and then check //if the goal string is a substring of this concatenated string.
 
        if (s.length() != goal.length()) // If lengths are different, return false
         return false;
@@ -9,7 +11,35 @@ class Solution {
     return concatenated.contains(goal);
             
 
+*/
 
+// Brute Force :
+
+        int len1= s.length();
+
+        int len2 = goal.length();
+
+        if(len1!=len2)  return false;
+
+
+        for(int i =0;i<len1;i++)
+        {
+            s= s.charAt(len1-1)+s.substring(0,len1-1);
+            //s=shiftRight(s)
+            if(s.equals(goal))
+            {
+                    return true;
+            }
+            
+
+            
+        }
+        return false;
         
     }
+
+   // public String shiftRight(String s){
+       // return s.CharAt(len1-1)+s.substring(0,len1-1);}
+
+
 }
