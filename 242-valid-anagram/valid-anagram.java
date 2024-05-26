95c7de3503b46c01f78c8cf5905e3ce959f264d5
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-26 19:47:16 +0530

    Time: 1 ms (99.81%) | Memory: 43 MB (72.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -32,7 +32,7 @@ class Solution {
 
 // Using HashMap :
 
-// 
+/*
 
 HashMap<Character,Integer> mpp = new HashMap<>();
 
@@ -73,11 +73,34 @@ return true;
 }
 
 
-
+*/
 
 
 
 // Using Hash Table :
+
+
+int hash[]= new int[26];
+
+for(char c : s.toCharArray())
+{
+    hash[c-'a']++;
+}
+
+for(char c : t.toCharArray())
+    {
+        hash[c-'a']--;
+    }
+
+    for(int i = 0;i<26;i++)
+    {
+        if(hash[i]!=0)
+        return false;
+    }
+
+    return true;
+    }
+}
 
 
 
