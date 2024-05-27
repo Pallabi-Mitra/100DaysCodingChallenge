f000b8c742d50c3d86bf68c9b6a81d758efde055
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-27 23:05:02 +0530

    Time: 11 ms (33.44%) | Memory: 45.5 MB (8.40%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -5,55 +5,31 @@ class Solution {
 
 //Follow-up: If the string data type is mutable in your language, can you solve it in-place 
 //with O(1) extra space?
+// Convert string to char array
 
-   // Convert the string to a char array
-    char[] chars = s.toCharArray();
-    int n = chars.length;
-    
-    // Reverse the entire char array
-    reverse(chars, 0, n - 1);
-    
-    // Reverse each word in the reversed char array
-    int start = 0;
-    for (int end = 0; end < n; end++) {
-        if (chars[end] == ' ') {
-            reverse(chars, start, end - 1);
-            start = end + 1;
-        }
+     String result = new String();
+    int i = 0;
+    int n = s.length();
+
+    while(i < n){
+        while(i < n && s.charAt(i) == ' ') i++;
+        if(i >= n) break;
+        int j = i + 1;
+        while(j < n && s.charAt(j) != ' ') j++;
+        String sub = s.substring(i, j);
+        if(result.length() == 0) result = sub;
+        else result = sub + " " + result;
+        i = j+1;
     }
-    // Reverse the last word
-    reverse(chars, start, n - 1);
-    
-    // Clean up spaces: remove leading, trailing, and extra spaces
-    return cleanSpaces(chars, n);
+    return result;
+}
 }
 
-private void reverse(char[] chars, int left, int right) {
-    while (left < right) {
-        char temp = chars[left];
-        chars[left] = chars[right];
-        chars[right] = temp;
-        left++;
-        right--;
-    }
-}
+// Stack Solution :
 
-private String cleanSpaces(char[] chars, int n) {
-    int i = 0, j = 0;
-    
-    while (j < n) {
-        // Skip leading spaces
-        while (j < n && chars[j] == ' ') j++;
-        // Copy non-space characters
-        while (j < n && chars[j] != ' ') chars[i++] = chars[j++];
-        // Skip spaces between words, but keep one space
-        while (j < n && chars[j] == ' ') j++;
-        if (j < n) chars[i++] = ' ';
-    }
-    
-    return new String(chars, 0, i);
-}
-}
+
+
+
 //Bruteforce :
 /*
 
