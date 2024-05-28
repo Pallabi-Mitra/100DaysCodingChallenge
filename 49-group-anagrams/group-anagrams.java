aff382ca761e11ae4e9f00d14e7f19fe4146235f
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-28 22:42:49 +0530

    Time: 18 ms (21.25%) | Memory: 48.4 MB (14.79%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            // Create a character count array
+            int[] charCount = new int[26];
+            for (char c : s.toCharArray()) {
+                charCount[c - 'a']++;
+            }
+
+            // Convert character count array to a string key
+            StringBuilder sb = new StringBuilder();
+            for (int count : charCount) {
+                sb.append('#');
+                sb.append(count);
+            }
+            String key = sb.toString();
+
+            // Add the original string to the corresponding list in the map
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+   
+        
+    }
+}
