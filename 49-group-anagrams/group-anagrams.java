f5613e0d926ea0e3fc6f5a99cb83871bd38db24c
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-28 22:43:33 +0530

    Time: 6 ms (97.72%) | Memory: 47.9 MB (29.93%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
+ /*
         if (strs == null || strs.length == 0) {
             return new ArrayList<>();
         }
@@ -30,7 +31,26 @@ class Solution {
         }
 
         return new ArrayList<>(map.values());
-   
+   */
+
+   if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(s);
+        }
+
+        return new ArrayList<>(map.values());
         
     }
 }
