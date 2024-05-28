20c47e26e4b06a028dab1858aac1f9429102e6e9
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-28 17:18:30 +0530

    Time: 12 ms (83.63%) | Memory: 45.7 MB (36.46%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,29 +1,67 @@
 class Solution {
     public String frequencySort(String s) {
-/*
 
-        HashMap<Character,Integer> mpp = new HashMap<>();
+/*
+    TreeMap<Integer,Character> mpp = new TreeMap<>(Collections.reverseOrder());
 
         for(char c : s.toCharArray())
         {
             int freq=0;
-            if(mpp.containsKey(c))
+
+            for(Map.Entry<Integer,Character> entries : mpp.entrySet())
             {
-                freq = mpp.get(c);
-                freq++;
+                if(entries.getValue()==c)
+                {
+                    freq = entries.getKey();
+                    freq++;
+                    
+                }
             }
-            mpp.put(c,freq);
+            
+            mpp.put(freq,c);
 
         }
 
-        for(char c : s.toCharArray)
-    {
-        if(mpp.get(c))
-    }
+        StringBuilder result = new StringBuilder();
+
+         for (Map.Entry<Integer,Character> entry : mpp.entrySet()) {
+            int f = entry.getKey();
+            char c = entry.getValue();
+            for (int i = 0; i < f; i++) {
+                result.append(c);
+            }
+        }
+
+
+        return result.toString();
         
         }
 }
 */
+ // Step 1: Count the frequency of each character
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+
+        // Step 2: Convert the HashMap entries to a list and sort it by frequency in descending order
+        List<Map.Entry<Character, Integer>> entryList = new ArrayList<>(frequencyMap.entrySet());
+        entryList.sort((entry1, entry2) -> entry2.getValue() - entry1.getValue());
+
+        // Step 3: Build the result string
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : entryList) {
+            char c = entry.getKey();
+            int freq = entry.getValue();
+            for (int i = 0; i < freq; i++) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+}
+/*
 // Step 1: Create a HashMap to store character frequency
         Map<Character, Integer> frequencyMap = new HashMap<>();
         
@@ -33,7 +71,7 @@ class Solution {
         }
         
         // Step 3: Sort the entries based on frequency
-        List<Map.Entry<Character, Integer>> entryList = new ArrayList<>(frequencyMap.entrySet());
+    List<Map.Entry<Character, Integer>> entryList = new ArrayList<>(frequencyMap.entrySet());
         Collections.sort(entryList, (a, b) -> b.getValue() - a.getValue());
         
         // Step 4: Construct the result string
@@ -49,3 +87,5 @@ class Solution {
         return result.toString();
     }
 }
+
+*/
