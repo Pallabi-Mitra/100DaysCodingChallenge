569647244a32f80c436feceb209aa096c49a33ec
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-29 19:08:33 +0530

    Time: 9 ms (96.92%) | Memory: 44.9 MB (53.17%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,9 +1,53 @@
 class Solution {
     public String longestPalindrome(String s) {
+ if (s == null || s.length() < 1) {
+            return "";
+        }
+
+        // Expansion Around Center
+        int maxLength = 0; // To track the length of the longest palindrome found
+        String res = ""; // To store the longest palindromic substring
+
+        int n = s.length();
+        char[] ch = s.toCharArray(); // Convert input string to a character array for easier access
+
+        int left, right;
+
+        for (int i = 0; i < n; i++) {
+            // Odd length palindromes
+            left = i;
+            right = i;
+
+            while (left >= 0 && right < n && ch[left] == ch[right]) {
+                if ((right - left + 1) > maxLength) {
+                    res = s.substring(left, right + 1); // Update the result with the new longest palindrome
+                    maxLength = right - left + 1;
+                }
+                left--; // Move left pointer outward
+                right++; // Move right pointer outward
+            }
+
+            // Even length palindromes
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < n && ch[left] == ch[right]) {
+                if ((right - left + 1) > maxLength) {
+                    res = s.substring(left, right + 1); // Update the result with the new longest palindrome
+                    maxLength = right - left + 1;
+                }
+                left--; // Move left pointer outward
+                right++; // Move right pointer outward
+            }
+        }
+        return res; // Return the longest palindromic substring found
+    
+    }
+}
 
 
 // Bruteforce : O(n3):
-
+/*
 
     int n = s.length();
     if (n < 2) {
@@ -37,3 +81,4 @@ private boolean isPalindrome(String s, int low, int high) {
         
     }
 }
+*/
