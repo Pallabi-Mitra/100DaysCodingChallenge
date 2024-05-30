0664ba6aaabc07432357333f7873588421e8ab1d
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-30 14:35:27 +0530

    Time: 2576 ms (5.01%) | Memory: 47 MB (49.60%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,6 +1,10 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
 
+
+// Sliding Window  : 
+
+/*
         int zeroes = 0;
             int right = 0;
             int left = 0;
@@ -25,6 +29,30 @@ class Solution {
                
                 maxlen = Math.max(maxlen,right-left+1);
                  right++;
+            }
+            return maxlen;
+            */
+
+
+            // Bruteforce :
+            int n = nums.length;
+            int maxlen = 0;
+            
+
+            for(int i = 0;i<n;i++)
+            {
+                int zeroes = 0;
+                for(int j = i;j<n;j++)
+                {
+                    if(nums[j]==0)
+                    zeroes ++;
+
+                    if(zeroes > k)
+                    break;
+                
+                maxlen = Math.max(maxlen,j-i+1);
+                }
+                
             }
             return maxlen;
     }
