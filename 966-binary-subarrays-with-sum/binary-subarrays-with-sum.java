d66d0cd77b5142603c48a3996953d854ca2fd39f
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-31 23:34:10 +0530

    Time: 1 ms (100.00%) | Memory: 47 MB (96.38%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,34 +1,31 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    public int numSubarraysWithSum(int[] nums, int S) {
+
+// Sliding Window :
+
+
+    return atMost(nums, S) - atMost(nums, S - 1);
+}
+
+private int atMost(int[] nums, int S) {
+    if (S < 0) return 0;
+    int count = 0;
+    int left = 0, sum = 0;
+    for (int right = 0; right < nums.length; right++) {
+        sum += nums[right]; // Expand the window
+        while (sum > S) {
+            sum -= nums[left++]; // Shrink the window
+        }
+        count += right - left + 1; // Number of subarrays ending at 'right'
+    }
+    return count; // Return the count of subarrays with sum at most S
+}
+}
+
 
   // Hashing : 
-/*
-  HashMap<Integer,Integer> mpp = new HashMap<>();
-  int n = nums.length;
-  int sum = 0;
-  int count =0;
 
-  for(int i = 0;i<n ; i++)
-  {
-       sum += nums[i];
-
-       int reqd = goal-sum;
-
-       if(mpp.containsKey(reqd))
-       {
-            count ++;
-       }
-
-       mpp.put(sum,i);
-  }
-  
-  return count;
-    }
-}
-  
-  */
-  
-  
+  /*
  // Initialize a hashmap to store the frequency of prefix sums
     HashMap<Integer, Integer> prefixSumCount = new HashMap<>();
     // Initialize the count of subarrays with sum equal to goal
@@ -56,7 +53,7 @@ class Solution {
     return count;
     }
 }
-  
+  */
         // Bruteforce : O(n^2):
         /*
 
