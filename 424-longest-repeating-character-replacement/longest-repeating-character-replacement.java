fe4f4b053393731664291300c3aa38ebb7758be8
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-05-31 16:22:46 +0530

    Time: 8 ms (76.16%) | Memory: 42.4 MB (69.95%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,8 +1,61 @@
 class Solution {
     public int characterReplacement(String s, int k) {
 
-// Bruteforce : 
 
+// Optimized Sliding Window Technique : O(n)
+
+int n = s.length();
+    int maxlen = 0;
+    int left = 0;
+    int right=0;
+    int window_size=0;
+    // maxFreq will store the count of the most frequent character in the current window
+    int maxFreq  = 0;
+    // Initialize an array to count the frequency of each character in the window
+    int hash[]= new int[26];
+
+// Use two pointers to represent the window, left and right
+    while(right < n)
+    {
+        // Increment the count of the character at the 'right' pointer in hash table
+        hash[s.charAt(right)-'A']++;
+
+// Update maxFreq with the frequency of the most frequent character in the current window
+        maxFreq = Math.max(maxFreq,hash[s.charAt(right)-'A']);
+
+// Calculate the current window size
+        window_size = right-left+1;
+
+ // If the number of replacements needed exceeds k, shrink the window from the left
+        if(window_size - maxFreq > k)
+        {
+            hash[s.charAt(left)-'A']--;
+            left++;
+        }
+
+       
+// Update maxLength with the size of the current valid window
+       if(window_size - maxFreq <=k)
+       {
+             maxlen= Math.max(maxlen,window_size);
+       }
+       
+        right++;
+
+    }
+
+ // Return the maximum length of the valid window found
+return maxlen;
+    }
+}
+
+
+
+
+
+
+// Bruteforce : O(n^2)
+/*
 
     int n = s.length();
     int maxlen = 0;
@@ -35,3 +88,5 @@ class Solution {
       return maxlen;  
     }
 }
+
+*/
