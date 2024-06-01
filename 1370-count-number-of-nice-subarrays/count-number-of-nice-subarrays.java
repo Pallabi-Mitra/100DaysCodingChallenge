65d5e908653168f40bd2693d5a9b6938b53dd819
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-06-01 13:24:34 +0530

    Time: 11 ms (67.87%) | Memory: 55.6 MB (7.47%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,59 +1,11 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
 
-// Hashing :
 
-
-
-    HashMap<Integer,Integer> mpp = new HashMap<>();
-    mpp.put(0,1);
-    int oddCount = 0;
-    int result = 0;
-
-
-    for(int num : nums)
-    {
-        if(num%2!=0)
-        oddCount++;
-
-        int needed = oddCount - k;
-
-        if(mpp.containsKey(needed))
-        {
-           result+= mpp.get(needed);
-        }
-
-
-
-        mpp.put(oddCount,mpp.getOrDefault(oddCount,0)+1);
-
-    }
-
-return result;
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // Sliding Window :
-        // Consider all even number as 0, and odd number as 1
-        // Same as finding the sum in the problem Binary subarrays with sum
+// Sliding Window :
 
 /*
-
-        int n = nums.length;
+ int n = nums.length;
         int subarray = 0;
         int count = 0;
         int right = 0;
@@ -84,8 +36,82 @@ return result;
 return subarray;
     }
 }
+*/
+// Hashing :
+
+/*
+
+    HashMap<Integer,Integer> mpp = new HashMap<>();
+    mpp.put(0,1);
+    int oddCount = 0;
+    int result = 0;
+
+
+    for(int num : nums)
+    {
+        // if odd number increase the odd count
+        if(num%2!=0)
+        oddCount++;
+
+        int needed = oddCount - k;
+// if the required is present in map, the value will be the no of subarrays,
+// because the freq stored is that many times the no is present in array, so can form desired subarray
+     
+       // Check if there is a subarray (ending here) with k odd numbers
+        if(mpp.containsKey(needed))
+        {
+           result+= mpp.get(needed);
+        }
+
+
+// Update the hash map with the current count of odd numbers
+        mpp.put(oddCount,mpp.getOrDefault(oddCount,0)+1);
+
+    }
+
+return result;
+    }
+}
 
 */
+
+
+
+
+        // Sliding Window :
+        // Consider all even number as 0, and odd number as 1
+        // Same as finding the sum in the problem Binary subarrays with sum
+
+
+
+       
+    return atMost(nums, k) - atMost(nums, k - 1);
+}
+
+// if odd number  : num %2 == 1
+// if even number : num %2 ==0
+ // it acts same as the binary array
+ // we can find sum of the odd numbers each 1 to match with k no of odd numbers
+private int atMost(int[] nums, int k) {
+    if (k < 0) return 0;
+    int count = 0;
+    int left = 0, sum = 0;
+    for (int right = 0; right < nums.length; right++) {
+        sum += nums[right]%2; // odd number sum
+        while (sum > k) {
+            sum -= nums[left++]%2; // Shrink the window
+        }
+        count += right - left + 1; // Number of subarrays ending at 'right'
+    }
+    return count; // Return the count of subarrays with sum at most S
+}
+}
+
+
+
+
+
+
 
 
 
