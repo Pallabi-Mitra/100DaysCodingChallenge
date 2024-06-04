9b9347f03a21c4c6c73a1e8d66bc0d9417cc7923
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-06-04 15:26:51 +0530

    Time: 1 ms (100.00%) | Memory: 41.9 MB (35.32%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,56 +1,61 @@
 class Solution {
     public int longestPalindrome(String s) {
 
-/*
-        HashMap<Character,Integer> mpp = new HashMap<>();
-        int count = 0;
-       // boolean odd_flag = false;
-        int i = 0;
 
 
-       // for(ch : s.toCharArray())
-       for(;i<s.length();i++)
-        {
-            mpp.put(s.charAt(i),mpp.getOrDefault(s.charAt(i),0)+1);
-        }
+// Most optimal :
 
-        if(mpp.size()==1)
-        {
-            count = mpp.get(s.charAt(i-1));
-            return count;
-        }
 
-       // for(int n : mpp.values())
+int count[]=new int[58];
+// ch -'A' // if lower case : 97-65 = 32 --- 58
+// ch - 'A' // if upper case : 65-65= 0 -----26
+int res = 0;
+int odd = 0;
 
-       for(Map.Entry<Character,Integer> entry : mpp.entrySet())
-        {
-            if(entry.getValue()%2==0)
-            {
-                count+=entry.getValue();
-            }
-            else
-            {
-               
-               
-                count+=entry.getValue()-1;
+for(char ch : s.toCharArray())
+{
+    count[ch-'A']++;
+}
 
-                
-               
-                mpp.put(entry.getKey(),entry.getValue()%2);
-            }
-            
-        }
-                 count++;
-        
-        
-
-        return count;
-
-        
+for(int n : count)
+{
+    if(n%2==0 && n!=0) // even and no empty spots
+    {
+        res +=n;
+    }
+    else if(n%2==1 && n !=1) // odd numbers greater than 1
+    {
+        res+=n-1; // take the even pair out , eg : 5 -> 5-1 = 4 chars can be taken
+        odd =1; // no of odd will be 1 no matter how many present
+    }
+    else if(n==1)
+    {
+        odd=1; // if only 1 char is present we can take it once, 
     }
 }
-*/
 
+
+return res + odd;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 9ms : 
+
+/*
         HashMap<Character,Integer> mpp = new HashMap<>();
         int count = 0;
        // boolean odd_flag = false;
@@ -82,11 +87,8 @@ class Solution {
             else
             {
                
-               
                 count+=entry.getValue()-1;
-
-                
-               
+ 
                 mpp.put(entry.getKey(),entry.getValue()%2);
             }
             
@@ -109,3 +111,70 @@ class Solution {
         return count;
     }
 }
+
+*/
+
+// 9ms : Lambda Expression :
+/*
+HashMap<Character,Integer> mpp = new HashMap<>();
+int count = 0;
+int i = 0;
+
+
+
+for(;i<s.length();i++)
+{
+    mpp.put(s.charAt(i),mpp.getOrDefault(s.charAt(i),0)+1);
+}
+
+if(mpp.size()==1)
+{
+    count = mpp.get(s.charAt(i-1));
+    return count;
+}
+
+
+// Iterator 
+/*
+Iterator<Map.Entry<Character,Integer>> index = mpp.entrySet().iterator();
+while (index.hasNext()) {
+    Map.Entry<Character,Integer> entry = index.next();
+    if(entry.getValue()%2==0){
+        count+=entry.getValue();
+        index.remove();
+    }
+
+ 
+
+ // Lambda Expression - java 8 :
+
+ //mpp.entrySet().removeIf(entry.getValue()%2==0);
+        //count+=entry.getValue();
+
+
+    else
+    {
+       
+       
+        count+=entry.getValue()-1;
+  
+        mpp.put(entry.getKey(),entry.getValue()%2);
+    }
+    
+}
+
+// just for 1 value :
+if(mpp.size()>0)
+{
+    
+        count++;
+     
+}
+
+return count;
+    }
+}
+*/
+
+
+
