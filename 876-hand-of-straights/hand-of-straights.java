f6d5b2a66d9ad3ca2315bed47c448e9b10b77328
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-06-06 14:57:56 +0530

    Time: 8 ms (100.00%) | Memory: 44.7 MB (97.30%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isNStraightHand(int[] nums, int k) {
 
 // HAshMap :
-
+/*
     // Edge case: if the array length is not a multiple of k, return false
         if (nums.length % k != 0) {
             return false;
@@ -36,11 +36,57 @@ class Solution {
         return true;
     }
 }
+*/
+  Arrays.sort(nums);
+        int n = nums.length;
+        if(n%k!=0) return false; // array cannot be grouped completely, elements will remain
+
+        
+
+        for(int i = 0;i<n;i++) // check for all array elemnts
+        {
+           if(nums[i]>=0) // if the no is -1 that is already taken in a set
+           {
+            if(!findNext(nums,k,i,n)) // if returns false cannot form set
+            return false;
+           
+           }
+        }
+
+        return true; // if never false encountered, it is true
+        
+    }
+
+    public boolean findNext(int nums[],int k,int i,int n)
+    {
+           
+           
+            int search = nums[i]+1; // consecutive no needs to be searched
+            nums[i]=-1; // set the current no as -1 in array
+            i+=1; // move to next
+            int count = 1; // to track the group size
+
+            while(i < n && count < k) // till group size
+            {
+                if(nums[i]==search) // if match found
+                {
+                    search = nums[i]+1; // move to next consecutive element search till group size not formed
+                    nums[i]=-1; // set the current as -1
+                    count++;
+                }
+                i++; //  check next element
+            }
+
+            if( count!=k) // if all matching consecutive elemnets counting to group size not found
+            return false;
+            else // group size matching consecutives found
+            return true;
+    }
+}
 
 
 
-
-// TreeMap :
+// TreeMap : No need to sort
 /*
 
 // since group size not possible to create
