a0aaa5b12c0a992b4abb10b56020495f072f8ae1
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-07-02 20:12:20 +0530

    Time: 3 ms (90.95%) | Memory: 42.9 MB (67.23%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -2,6 +2,44 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
 
 
+
+        // Step 1: Sort both input arrays
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        // Step 2: Initialize variables
+        List<Integer> result = new ArrayList<>();
+        int i = 0, j = 0;
+        
+        // Step 3: Iterate through sorted arrays using two pointers
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                // Found intersection, add to result list
+                result.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                // Move pointer of nums1 to the right
+                i++;
+            } else {
+                // Move pointer of nums2 to the right
+                j++;
+            }
+        }
+        
+        // Step 4: Convert list to array
+        int[] intersection = new int[result.size()];
+        for (int k = 0; k < result.size(); k++) {
+            intersection[k] = result.get(k);
+        }
+        
+        // Step 5: Return the intersection array
+        return intersection;
+    }
+}
+
+// Brute force :
+/*
         ArrayList<Integer> al = new ArrayList<>();
 
         int k =0;
@@ -29,3 +67,5 @@ class Solution {
         
     }
 }
+
+*/
