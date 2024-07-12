7108af69789d285b7b06b2b95442097de704f0ef
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-07-12 23:07:09 +0530

    Time: 56 ms (80.81%) | Memory: 45.7 MB (62.63%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -1,39 +1,43 @@
 class Solution {
     public int maximumGain(String s, int x, int y) {
- // Helper function to process removals
-    return x > y ? process(s, x, y, 'a', 'b') : process(s, y, x, 'b', 'a');
-        
-    }
+        int res = 0;
+        String top, bot;
+        int top_score, bot_score;
 
-    private int process(String s, int firstVal, int secondVal, char firstChar, char secondChar) {
-    Stack<Character> stack = new Stack<>();
-    int score = 0;
-
-    // First pass: Remove higher priority pair
-    for (char c : s.toCharArray()) {
-        if (!stack.isEmpty() && stack.peek() == firstChar && c == secondChar) {
-            stack.pop();
-            score += firstVal;
+        if (y > x) {
+            top = "ba";
+            top_score = y;
+            bot = "ab";
+            bot_score = x;
         } else {
-            stack.push(c);
+            top = "ab";
+            top_score = x;
+            bot = "ba";
+            bot_score = y;
         }
-    }
 
-    // Second pass: Convert remaining stack to string and process lower priority pair
-    StringBuilder remainingString = new StringBuilder();
-    while (!stack.isEmpty()) {
-        remainingString.insert(0, stack.pop());
-    }
-
-    for (char c : remainingString.toString().toCharArray()) {
-        if (!stack.isEmpty() && stack.peek() == secondChar && c == firstChar) {
-            stack.pop();
-            score += secondVal;
-        } else {
-            stack.push(c);
+        // Removing first top substrings cause they give more points
+        StringBuilder stack = new StringBuilder();
+        for (char ch : s.toCharArray()) { // Changed 'char' to 'ch'
+            if (ch == top.charAt(1) && stack.length() > 0 && stack.charAt(stack.length() - 1) == top.charAt(0)) {
+                res += top_score;
+                stack.setLength(stack.length() - 1);
+            } else {
+                stack.append(ch);
+            }
         }
-    }
 
-    return score;
-}
+        // Removing bot substrings cause they give less or equal amount of scores
+        StringBuilder new_stack = new StringBuilder();
+        for (char ch : stack.toString().toCharArray()) { // Changed 'char' to 'ch'
+            if (ch == bot.charAt(1) && new_stack.length() > 0 && new_stack.charAt(new_stack.length() - 1) == bot.charAt(0)) {
+                res += bot_score;
+                new_stack.setLength(new_stack.length() - 1);
+            } else {
+                new_stack.append(ch);
+            }
+        }
+
+        return res;
+    }
 }
