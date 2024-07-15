2370958feaba892fa5edbdcad6625c898ccd2d5a
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-07-15 08:48:46 +0530

    Time: 66 ms (65.23%) | Memory: 55.2 MB (86.75%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -16,7 +16,7 @@
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
 
-
+/*
   Map<Integer, TreeNode> nodeMap = new HashMap<>();
         Set<Integer> childSet = new HashSet<>();
         
@@ -55,5 +55,37 @@ class Solution {
         }
         
         return root;
+    }
+}
+
+*/
+
+   Map<Integer, TreeNode> nodeMap = new HashMap<>();
+        Set<Integer> childSet = new HashSet<>();
+        
+        for (int[] desc : descriptions) {
+            int parent = desc[0];
+            int child = desc[1];
+            boolean isLeft = desc[2] == 1;
+
+            nodeMap.putIfAbsent(parent, new TreeNode(parent));
+            nodeMap.putIfAbsent(child, new TreeNode(child));
+
+            if (isLeft) {
+                nodeMap.get(parent).left = nodeMap.get(child);
+            } else {
+                nodeMap.get(parent).right = nodeMap.get(child);
+            }
+
+            childSet.add(child);
+        }
+        
+        for (int[] desc : descriptions) {
+            if (!childSet.contains(desc[0])) {
+                return nodeMap.get(desc[0]);
+            }
+        }
+        
+        return null;
     }
 }
