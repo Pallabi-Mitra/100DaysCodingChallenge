50e521083efa29cd9407e2b68549f1e4543818fc
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-07-18 00:59:48 +0530

    Time: 9 ms (17.81%) | Memory: 42.7 MB (27.96%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -19,6 +19,9 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
 
+
+        /*
+
         List<String> ans = new ArrayList<>();
 
         if(root ==null)
@@ -26,47 +29,74 @@ class Solution {
             return ans; // return an empty list
         }
 
-        checkPath(root,"",ans); // TreeNode root, String path, List<String> ans
-        return ans;
+        return checkPath(root,ans);
   
     }
 
-    public void checkPath(TreeNode root, String path, List<String> ans)
+    public List<String> checkPath(TreeNode root, List<String> ans)
     {
 
         // base condition :
-/*
+
         if(root.left ==null && root.right ==null) // leaf node
         {
            return ans.add(root);
         }
-*/
 
-        if(root ==null) // base condition 
-        {
-            return ;
-        }
+        ans.add(root);
 
-        //Append current node's value to the path :
 
-        path+= root.val;
 
-        // Check if the current node is a leaf node :
 
-        if(root.left ==null && root.right ==null) // leaf node
-        {
-
-            // Add the path to the List
-            ans.add(path);
-        }
-        else
-        {
-            //If not a leaf node, append a '->' and recurse
-            path+= "->";
-            checkPath(root.left,path,ans);
-            checkPath(root.right,path,ans);
-        }
-
-      
     }
 }
+*/
+// Iterative way :
+ List<String> paths = new ArrayList<>();
+        if (root == null) {
+            return paths;
+        }
+
+        Stack<Pair<TreeNode, String>> stack = new Stack<>();
+        stack.push(new Pair<>(root, String.valueOf(root.val)));
+
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, String> current = stack.pop();
+            TreeNode node = current.getKey();
+            String path = current.getValue();
+
+            if (node.left == null && node.right == null) {
+                paths.add(path);
+            } else {
+                if (node.right != null) {
+                    stack.push(new Pair<>(node.right, path + "->" + node.right.val));
+                }
+                if (node.left != null) {
+                    stack.push(new Pair<>(node.left, path + "->" + node.left.val));
+                }
+            }
+        }
+
+        return paths;
+    }
+
+    // Pair class to hold node and path string
+    class Pair<K, V> {
+        private K key;
+        private V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+}
+
