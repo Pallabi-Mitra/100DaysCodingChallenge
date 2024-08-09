434c91b2fc328d86d451e8a2a8fdee6330f61e2e
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-08-09 14:22:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (90.79%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        if(root.val==left.val+right.val)
+        return true;
+        else
+        return false;
+        
+    }
+}
