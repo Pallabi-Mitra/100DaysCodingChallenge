ed54897b30d9b31e079bfb6dbb0191e798230081
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-09-10 09:52:58 +0530

    Time: 11 ms (15.26%) | Memory: 46.2 MB (85.94%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -3,8 +3,51 @@
 
 class Solution {
     public int[][] merge(int[][] arr) {
+
+        List<int[]> res = new ArrayList<>();
+
+        if(arr.length==0 || arr==null)
+        {
+            return res.toArray(new int[0][]);
+        }
+
+        Arrays.sort(arr,(a,b)->a[0]-b[0]);
+
+        int start = arr[0][0];
+        int end = arr[0][1];
+
+
+        for(int[] i : arr)
+        {
+            if(i[0] <= end) // overlapping- start of next is less than prev end
+            {
+                end = Math.max(end, i[1]); // merging - taking max end
+            }
+            else // not merging so add normally
+            {
+                res.add(new int[]{start,end});
+                start=i[0]; // reassign the intervals 
+                end = i[1];
+            }
+        }
+
+
+        res.add(new int[]{start,end});
+
+        return res.toArray(new int[0][]);
+
+
+
+    }
+}
         
         // Optimal, 2D Array:
+
+        /*
+
+        class Solution {
+    public int[][] merge(int[][] arr) {
+        
         int n = arr.length;
          List<int []> ans = new ArrayList<>();
         Arrays.sort(arr, (a, b) -> Integer.compare(a[0],b[0]));
@@ -34,6 +77,7 @@ class Solution {
         }
 
     }
+    */
 
        /*
        // optimal : TC: O(n log n )+ O(n) : Using return list
