732517c72d103457ea3a8313909715797c6acc5f
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-09-17 00:06:33 +0530

    Time: 2 ms (91.23%) | Memory: 47.7 MB (7.36%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,6 +1,31 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
 
+        //using two pointers : for constant space constraint and sorted array space utility
+
+        int left = 0;
+        int right = numbers.length-1;
+
+        while(left < right)
+        {
+            int sum = numbers[left]+numbers[right];
+            if(sum==target)
+            {
+                return new int[]{left+1,right+1};
+            }
+            else if(sum < target)
+            {
+                    left++;
+            }
+            else
+            {
+                 right--;
+            }
+        }
+ return new int[] {-1, -1};
+//Your solution must use only constant extra space.: cant use hashmap. 
+
+/*
         HashMap<Integer,Integer> mpp = new HashMap<>();
         int ans[]=new int[2];
 
@@ -20,6 +45,9 @@ class Solution {
         }
 
         return ans;
+
+        */
+
         
     }
 }
