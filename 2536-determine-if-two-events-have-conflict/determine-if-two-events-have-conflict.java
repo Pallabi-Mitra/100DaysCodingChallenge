c3911af766c9186b0704c0030126e5879fe806bd
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-09-22 01:52:46 +0530

    Time: 1 ms (46.87%) | Memory: 41.1 MB (41.03%) - LeetSync

--- 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.java
+++ 2536-determine-if-two-events-have-conflict/determine-if-two-events-have-conflict.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean haveConflict(String[] event1, String[] event2) {
 
+
+/*
          // Convert the end time of event1 and start time of event2 to minutes
         int endTime1 = convertToMinutes(event1[1]);  // Event 1 end time (HH:MM to minutes)
         int startTime2 = convertToMinutes(event2[0]);  // Event 2 start time (HH:MM to minutes)
@@ -31,5 +33,15 @@ class Solution {
         return hours * 60 + minutes;  // Return total minutes since midnight
     }
         
+    */
+    Integer event1_0 = Integer.valueOf(event1[0].replace(":", ""));
+    Integer event1_1 = Integer.valueOf(event1[1].replace(":", ""));
+    Integer event2_0 = Integer.parseInt(event2[0].replace(":", ""));
+    Integer event2_1 = Integer.parseInt(event2[1].replace(":", ""));
     
+    if (event2_1 < event1_0) return false;
+    if (event1_1 >= event2_0) return true;
+    
+    return false;
+    }
 }
