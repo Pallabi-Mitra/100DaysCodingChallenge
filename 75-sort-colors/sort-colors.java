54f4e3ea3938c1ad1ea07d4caa7a5df6ebdd7667
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-11-14 11:02:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (49.67%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,110 +1,32 @@
 class Solution {
-    public void sortColors(int[] arr) {
+    public void sortColors(int[] nums) {
 
+        int count_0 = 0;
+        int count_1 = 0;
+        int count_2 = 0;
 
-// Deutch Flag Algorithm //
-/*
-        int n = arr.length;
-        int low = 0, mid = 0, high = n - 1; // 3 pointers
-
-        while (mid <= high) {
-            if (arr[mid] == 0) {
-                // swapping arr[low] and arr[mid]
-                int temp = arr[low];
-                arr[low]= arr[mid];
-                arr[mid] =  temp;
-
-                low++;
-                mid++;
-
-            } else if (arr[mid] == 1) {
-                mid++;
-
-            } else {
-                // swapping arr[mid] and arr[high]
-                int temp = arr[mid];
-                arr[mid]= arr[high];
-                arr[high]= temp;
-
-                high--;
-            }
-        }
-
-*/
-    /*
-                int n = arr.length;
-                
-               int low = 0;
-               int mid=0;
-               int high=n-1;
-
-
-               while(mid<=high)
-               {
-                    if(arr[mid]==0)
-                    {
-                           int temp=arr[mid];
-                            arr[mid]=arr[low];
-                            arr[low]=temp;
-                            low++;
-                            mid++;
-
-                    }
-                    else if(arr[mid]==1)
-                    {
-                        mid++;
-                    }
-
-                   else if(arr[mid]==2)
-                    {
-                       int temp=arr[mid];
-                        arr[mid]= arr[high];
-                        arr[high]=temp;
-                       high--;
-                    }
-               }
-
-               */
-        // Better Approach :
-
-
-        int count0 = 0;
-        int count1 = 0;
-        int count2= 0;
-
-        int n =arr.length;
-        int i;
-
-        for(i=0;i<n;i++)
+        for(int i = 0; i< nums.length;i++)
         {
-            if(arr[i]==0)
-            {
-                count0++;
-            }
-            else if (arr[i]==1)
-            {
-                count1++;
-            }
-            else
-            {
-                count2++;
-            }
+            if(nums[i]==0)
+                count_0++;
+            if(nums[i]==1)
+                count_1++;
+            if(nums[i]==2)
+                count_2++;
         }
 
-        for(i=0;i<count0;i++)
+        for(int i  =0;i<count_0;i++)
         {
-            arr[i]=0;
+            nums[i]=0;
         }
-
-        for(i=count0;i<count0+count1;i++)
+        for(int i = count_0;i<count_0+count_1;i++)
         {
-            arr[i]=1;
+            nums[i]=1;
         }
-        for(i=count1+count0;i<n;i++)
+        for(int i = count_0+count_1;i<nums.length;i++)
         {
-            arr[i]=2;
+            nums[i]=2;
         }
-
-    }
         
     }
+}
