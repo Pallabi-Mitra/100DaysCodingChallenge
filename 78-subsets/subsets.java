c30cc61dddcd93958d0d31873730c376f7b3d10d
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2024-12-04 01:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (71.45%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,36 +1,25 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
 
-        // Define the actual recursive function method :
-        List<List<Integer>> res = new ArrayList<>();
-        ArrayList<Integer> al = new ArrayList<>();
-       
+        // Bit manipulation - better than recursion due to less SC
 
-        solve(0,al,res,nums);
-        return res;
-    }
-
-    public void solve(int i,ArrayList<Integer> al,List<List<Integer>> res,int nums[])
-
-    {
-            //base condition :
-            if(i==nums.length)
+        int n = nums.length;
+        int subsets = 1<<n;
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0;i<subsets;i++)
+        {
+            List<Integer> al = new ArrayList<>();
+            for(int j=0;j<n;j++)
             {
-                res.add(new ArrayList<>(al));
-                return;
+                // check ith bit set or not
+                if( (i & (1<<j)) !=0) // 1
+                    al.add(nums[j]);
             }
 
-            // take condition :
+            ans.add(al);
+        }
 
-            al.add(nums[i]);
-            solve(i+1,al,res,nums);
-
-
-            // not take condition :
-            al.remove(al.size()-1);
-
-            solve(i+1,al,res,nums);
+        return ans;
+        
     }
-
 }
-
