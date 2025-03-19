eae8eba91f8412f1eae5646d501e2f4cf097addb
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2025-03-20 00:25:05 +0530

    Time: 3 ms (90.20%) | Memory: 44.9 MB (26.74%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,7 +2,10 @@ class Solution {
     public boolean isAnagram(String s, String t) {
 
 // By sorting 
-/*
+
+// TC : O(n log n) - sorting
+// SC : O(n) - For arrays
+
 
     char ch1[]= s.toCharArray();
 
@@ -23,84 +26,10 @@ class Solution {
     return true;
     else
     return false;
-    //return Arrays.equals(ch1, ch2);
-      
+
+
+
+
+        
     }
 }
-
-*/
-
-// Using HashMap :
-
-/*
-
-HashMap<Character,Integer> mpp = new HashMap<>();
-
-
-// put the characters in 1st string along with their frequency in hashmap:
-
-for(char c: s.toCharArray())
-{
-    
-    mpp.put(c,mpp.getOrDefault(c,0)+1);
-
-
-}
-
-// iterate through string 2 and check if something is not present in hashmap or else reduce the count and put it
-
-for(char ch : t.toCharArray())
-{
-    if(!mpp.containsKey(ch))
-        return false;
-    
-    mpp.put(ch,mpp.get(ch)-1);
-}
-
-
-// looping through hashmap to check which is having value other than 0. It needs to be 0 to match completely
-
-
-for(int count : mpp.values())
-{
-    if(count!=0)
-    return false;
-    
-}
-
-return true;
-    }
-}
-
-
-*/
-
-
-
-// Using Hash Table :
-
-
-int hash[]= new int[26];
-
-for(char c : s.toCharArray())
-{
-    hash[c-'a']++;
-}
-
-for(char c : t.toCharArray())
-    {
-        hash[c-'a']--;
-    }
-
-    for(int i = 0;i<26;i++)
-    {
-        if(hash[i]!=0)
-        return false;
-    }
-
-    return true;
-    }
-}
-
-
-
