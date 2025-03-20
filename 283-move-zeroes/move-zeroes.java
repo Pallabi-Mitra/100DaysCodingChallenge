ff6355ba34a79eb9cc80c8c02b03f1c62e093d82
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2025-03-20 16:46:48 +0530

    Time: 2 ms (84.88%) | Memory: 45.8 MB (80.49%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+
+        // Bruteforce :
+
+        int n = nums.length;
+        int arr[]= new int[n];
+        int noOfZero =0;
+        int index=0;
+
+        for(int i=0; i<n ;i++)
+        {
+            if(nums[i]!=0)
+            {
+                arr[index]=nums[i];
+                index++;
+            }
+           
+
+        }
+
+        for(int i =0;i<n;i++)
+        {
+            nums[i]=arr[i];
+        }
+
+
+        
+    }
+}
