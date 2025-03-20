a2167ea43058cbbdf5ece333d6cf2bc12dcf1ee1
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2025-03-20 22:51:02 +0530

    Time: 2 ms (84.88%) | Memory: 45.9 MB (80.49%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -7,6 +7,8 @@ class Solution {
         //TC: O(n)
         //SC: O(n)
 
+        /*
+
         int n = nums.length;
         int arr[]= new int[n];
         int noOfZero =0;
@@ -28,7 +30,46 @@ class Solution {
             nums[i]=arr[i];
         }
 
+*/
 
-        
+    //Optimized :
+
+    // SC: O(1):
+
+    int n= nums.length;
+    int p1 = 0;
+    int p2 = 0;
+
+    
+    while(p2 <n)
+    {
+         if(nums[p2]!=0)
+        {
+            int swap = nums[p2];
+            nums[p2]=nums[p1];
+            nums[p1]=swap;
+            p1++;
+           // p2++;
+        }
+    p2++;
+
     }
+        
+
+        /*
+       int nonZero = 0;  
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[nonZero], nums[i]);
+                nonZero++;
+            }
+        }
+
+        */
+
+    }
+
+    
+        
+    
 }
