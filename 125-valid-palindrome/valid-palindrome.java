a724eec9602fbe4357e983239db24563fa6f91e1
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2025-03-22 16:49:35 +0530

    Time: 13 ms (43.34%) | Memory: 45.4 MB (17.13%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,79 +2,30 @@ class Solution {
     public boolean isPalindrome(String s) {
 
 
-// Optimal :
+        s= s.replaceAll("[^a-zA-Z0-9]","");
+        s= s.toLowerCase();
+        char ch []= s.toCharArray();
 
-if (s.isEmpty()) {
-        	return true;
-        }
+
+
+        int n = ch.length;
+
         int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
+        int end = n-1;
+
+        while(start<end)
+        {
+            if(ch[start]!=ch[end])
+            {
+                return false;
+            }
+            else
+            {
+                start++;
+                end--;
+            }
         }
+        
         return true;
     }
 }
-
-
-// Using String Builder //
-
-/*
-StringBuilder sb = new StringBuilder();
-
-    for(char ch : s.toCharArray()){
-        if(Character.isLetterOrDigit(ch)){
-            sb.append(Character.toLowerCase(ch));
-        }
-    }
-    if(sb.toString().equals(sb.reverse().toString())){
-    return true;
-    }
-    return false;
-    }
-}
-*/
-
-// 2-pointer approach :
-/*
-    String ans = "";
-
-    for(char c : s.toCharArray())
-    {
-        if(Character.isDigit(c) || Character.isLetter(c))
-        {
-            ans += c;
-        }
-
-    }
-    ans=ans.toLowerCase();
-
-    int begin = 0;
-    int end = ans.length()-1;
-
-    while(begin<=end)
-    {
-        if(ans.charAt(begin)!=ans.charAt(end))
-            return false;
-        begin++;
-        end--;
-    }
-
-    return true;
-     
-    }
-}
-
-*/
