a2196a21a117da6a04a6ca1be28b65950eb82081
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2025-08-07 19:03:46 +0530

    Time: 18 ms (5.21%) | Memory: 44.9 MB (16.73%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.java
@@ -1,6 +1,8 @@
 class Solution {
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
 
+
+/*
         int n = fruits.length;
         
         int count = n;
@@ -19,6 +21,38 @@ class Solution {
         }
 
         return count;
+
+        */
+
         
+        int n = fruits.length;
+        TreeSet<Integer> available = new TreeSet<>(); // holds indices of available baskets
+
+        // Use TreeMap to map index -> capacity
+        Map<Integer, Integer> basketCapacities = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            available.add(i); // store basket index
+            basketCapacities.put(i, baskets[i]);
+        }
+
+        int unplaced = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean placed = false;
+            for (int index : available) {
+                if (basketCapacities.get(index) >= fruits[i]) {
+                    available.remove(index); // mark as used
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+
+        return unplaced;
     }
 }
+
+   
