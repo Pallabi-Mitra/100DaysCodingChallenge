6ded2ce27de4829cff6a355ba93b8774e7ca1d2a
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2025-12-22 13:09:11 -0500

    Time: 0 ms (100.00%) | Memory: 44.1 MB (13.96%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,89 +1,45 @@
-class Solution {
-   
-// Optimized : Binary Search :
 
-public boolean binarySearch(int []matrix, int target)
-{
-            int low = 0;
-            int high = matrix.length;
-
-            while(low<=high)
-            {
-                int mid = (low+high)/2;
-
-                if(matrix[mid]==target)
-                {
-                    return true;
-                }
-                else if(matrix[mid]<target)
-                {
-                    low=mid+1;
-                }
-                else
-                {
-                    high = mid-1;
-                }
-            }
-            return false;
-            
-}
- public boolean searchMatrix(int[][] matrix, int target) {
-
-    int n = matrix.length;
-    int m = matrix[0].length;
-
-    for(int i = 0;i<n;i++)
-    {
-        if(matrix[i][0] <= target && matrix[i][m-1] >= target)
-        {
-            // start binary search :
-            /*
-            int low = 0;
-            int high = m-1;
-
-            while(low<=high)
-            {
-                int mid = (low+high)/2;
-
-                if(matrix[i][mid]==target)
-                {
-                    return true;
-                }
-                else if(matrix[i][mid]<target)
-                {
-                    low=mid+1;
-                }
-                else
-                {
-                    high = mid-1;
-                }
-            }
-            */
-            return binarySearch(matrix[i],target);
-        }
         
-    }
+        class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
 
-    return false;
+        int m = matrix.length; // rows
+
+        int n = matrix[0].length; // columns
 
 
+// flatten into 1D array :
 
+        int left = 0;
 
+        int right = m*n -1;
 
+       
 
-/* Bruteforce :
-
-    for(int i  =0 ; i<n;i++)
-    {
-        for(int j = 0;j<m;j++)
+        while(left<=right)
         {
-            if(matrix[i][j]==target)
-            {
+                int mid = left + (right-left)/2;
+
+                int row = mid/n;
+                int col = mid%n;
+
+                int value = matrix[row][col];
+
+
+                if(value == target)
+
                 return true;
-            }
+
+                else if(value < target)
+
+                left = mid+1;
+
+                else
+                 right = mid-1;
         }
-    }
-    return false;
-   */     
+
+       
+       return false;
+        
     }
 }
