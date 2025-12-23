49eac2bf96fe2021022bc4df9b230716e609687a
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2025-12-22 20:47:19 -0500

    Time: 6 ms (99.07%) | Memory: 49.7 MB (49.42%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,60 +1,37 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
- // Counting Array :
- /*
-        if (strs == null || strs.length == 0) {
-            return new ArrayList<>();
-        }
 
-        Map<String, List<String>> map = new HashMap<>();
+        // Using Hashmap : sorting the strings and storing as key value
 
-        for (String s : strs) {
-            // Create a character count array
-            int[] charCount = new int[26];
-            for (char c : s.toCharArray()) {
-                charCount[c - 'a']++;
-            }
+        Map<String,List<String>> map = new HashMap<>();
 
-            // Convert character count array to a string key
-            StringBuilder sb = new StringBuilder();
-            for (int count : charCount) {
-                sb.append('#');
-                sb.append(count);
-            }
-            String key = sb.toString();
+        for(String s : strs)
+        {
+            char ch[]= s.toCharArray();
+            //sort the characters 
 
-            // Add the original string to the corresponding list in the map
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
-            }
+            Arrays.sort(ch);
+
+            // converting the character array to string
+
+            String key = new String(ch);
+
+// putting in the hashmap :
+
+            map.putIfAbsent(key, new ArrayList<>());
+
             map.get(key).add(s);
+
+            
+
+
         }
 
         return new ArrayList<>(map.values());
-   */
 
 
-// Sorted Array :
 
-   if (strs == null || strs.length == 0) {
-            return new ArrayList<>();
-        }
 
-        Map<String, List<String>> map = new HashMap<>();
-
-        for (String s : strs) {
-            char[] charArray = s.toCharArray();
-            Arrays.sort(charArray);
-            String sortedStr = new String(charArray);
-
-            if (!map.containsKey(sortedStr)) {
-                map.put(sortedStr, new ArrayList<>());
-            }
-            map.get(sortedStr).add(s);
-        }
-
-        return new ArrayList<>(map.values());
-       
     }
 }
