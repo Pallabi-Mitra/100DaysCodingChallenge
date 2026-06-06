318dc5c250793aca1a6fe3ca42bbba697fca64c6
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2026-06-06 08:49:18 -0400

    Time: 21 ms (30.50%) | Memory: 80.8 MB (79.13%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,69 +1,25 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
+        int count = 0;
+        //HashMap<Integer,Integer> mpp = new HashMap<>();
+
+        HashSet<Integer> st = new HashSet<>();
         int n = nums.length;
-/*
-        for(int i = 0;i<n;i++)
+
+        for(int i=0; i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i]==nums[j])
-                {
-                    return true;
-                }
-            }
+            st.add(nums[i]);
+
+
         }
+
+
+        if(st.size()<n)
+            return true;
+
+        else
         return false;
-    */
-// HashSet :
-/*
-    HashSet<Integer> st = new HashSet<>();
-
-    for(int i = 0;i<n;i++)
-    {
-        st.add(nums[i]);
-    }    
-
-    if(st.size()<n)
-    {
-            return true;
-    }
-    else
-    {
-        return false;
-    }
-    */
-
-    // Hashset approach 1 : faster
-    /*
-
-    HashSet<Integer> seen = new HashSet<>();
-    for(int num : nums)
-    {
-        if(seen.contains(num))
-        {
-            return true;
-        }
-
-        seen.add(num);
-    }
-    return false;
-    */
-
-    //HashMap Approach :
-
-    HashMap<Integer,Integer> mpp = new HashMap<>();
-
-    for(int i = 0;i<n;i++)
-    {
-        if(mpp.containsKey(nums[i]) && mpp.get(nums[i])>=1)
-        {
-            return true;
-        }
-
-        mpp.put(nums[i], mpp.getOrDefault(nums[i],0)+1);
-    }
-
-    return false;
+        
     }
 }
