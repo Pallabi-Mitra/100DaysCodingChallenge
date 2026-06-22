1c4e6d359e9a7ccf91ddb1c7bdc99b68a4426f04
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2026-06-22 09:22:07 -0400

    Time: 4 ms (70.54%) | Memory: 21.1 MB (87.60%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,6 +1,33 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
+        # 'candidate' stores the number we currently suspect is the majority element.
+    # We initialize it to None because we haven't looked at any numbers yet.
+        candidate = None
+        
+        # 'count' tracks the "voting power" or net balance of our current candidate.
+        count = 0
+        
+        # Iterate through each number in the array
+        for num in nums:
+            # If the count drops to 0, it means the previous candidate was 
+            # completely canceled out by other numbers. We must pick a new candidate.
+            if count == 0:
+                candidate = num
+            
+            # If the current number matches our candidate, we increase their voting power.
+            if num == candidate:
+                count += 1
+            # If it's a different number, they "fight" and cancel each other out, 
+            # decreasing the candidate's voting power by 1.
+            else:
+                count -= 1
+                
+        # Because the majority element appears more than n // 2 times, it is 
+        # mathematically guaranteed to be the last candidate standing.
+        return candidate
+
+
         """
         count = {}
         res,maxCount = 0,0
@@ -13,6 +40,7 @@ class Solution:
         return res
         """
 
+"""
         count_map = {}
         threshold = len(nums) // 2  # This represents floor(n / 2)
     
@@ -26,3 +54,5 @@ class Solution:
             # Check if this number has crossed the majority threshold
             if count_map[num] > threshold:
                 return num
+
+                """
