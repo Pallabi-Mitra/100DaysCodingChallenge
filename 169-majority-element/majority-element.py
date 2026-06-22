11492166429976d837120bfa143eb36781cb783e
Pallabi <55991425+Pallabi-Mitra@users.noreply.github.com>
2026-06-22 08:59:16 -0400

    Time: 11 ms (33.00%) | Memory: 21.2 MB (20.31%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,5 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+
+        """
         count = {}
         res,maxCount = 0,0
 
@@ -9,3 +11,18 @@ class Solution:
             res = n if count[n] > maxCount else res
             maxCount = max(count[n],maxCount)
         return res
+        """
+
+        count_map = {}
+        threshold = len(nums) // 2  # This represents floor(n / 2)
+    
+        for num in nums:
+            # Update the frequency count of the number
+            if num in count_map:
+                count_map[num] += 1
+            else:
+                count_map[num] = 1
+            
+            # Check if this number has crossed the majority threshold
+            if count_map[num] > threshold:
+                return num
